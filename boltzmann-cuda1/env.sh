d7aacf90045377dd2f536dd6d31b8d3028238b9d
ah44apiq <youyi.zhang@fau.de>
2025-02-01 18:02:04 +0100

    clean redundant code

--- boltzmann-cuda1/env.sh
+++
@@ -1,6 +0,0 @@
-# Add any `module load` or `export` commands that your code needs to
-# compile and run to this file.
-
-module load cmake intelmpi python 
-srun --ntasks=2 ./d2q9-bgk test/input_128x128.params test/obstacles_128x128.dat 
-python check/check.py --ref-av-vels-file=check/128x128.av_vels.dat --ref-final-state-file=check/128x128.final_state.dat --av-vels-file=./av_vels.dat --final-state-file=./final_state.dat
--- mpi-long/test.sh
+++
@@ -1,8 +0,0 @@
-unset SLURM_EXPORT_ENV
-
-
-GPU=1
-N=900
-
-# nsys profile --stats=true \
-srun --ntasks=${GPU} ./jacobi -nx ${N} -ny ${N}
