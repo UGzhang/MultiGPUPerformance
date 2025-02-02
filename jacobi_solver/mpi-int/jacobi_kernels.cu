03d458ec70dd9c1fcbe8f6713e388e6c2b820bdc
ah44apiq <youyi.zhang@fau.de>
2025-02-02 04:00:42 +0100

    change script file in nvshemem jacobi

--- jacobi_solver/mpi-int/jacobi_kernels.cu
+++ jacobi_solver/mpi-int/jacobi_kernels.cu
@@ -1,4 +1,5 @@
 /* Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2025, Youyi Zhang. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
